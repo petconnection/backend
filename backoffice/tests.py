2a93798d5b1374afe7131183ca3df77f72e02436
Pablo <pablo.riutort@gmail.com>
2017-10-08 02:03:43 +0200

    Add login test proposal
    Testing for not logged, login redirection and login template view

--- backoffice/tests.py
+++ backoffice/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+
+class LoginTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user('testuser', 'test@test.org', '123')
+        self.client = Client()
+
+
+    def test_not_logged(self):
+        response = self.client.get('/')
+        self.assertRedirects(response, reverse('backoffice-login'))
+
+
+    def test_view(self):
+        response = self.client.get(reverse('backoffice-login'))
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.templates[0].name, 'backoffice/login.html')
+
+
+    def test_login(self):
+        response = self.client.post(reverse('backoffice-login'), {'username': 'testuser', 'password': '123'}, follow=True)
+        self.assertRedirects(response, '/')
+
