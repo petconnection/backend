72e59457bfe077dc0fb21cc41f4344a90ee74d0c
Carlos Lucas <nosk14@gmail.com>
2017-10-10 20:23:20 +0200

    Login page Made login page works and add some settings
    - Updatet template for login view
    - Changed urls.py to use the login provided by module auth
    - Added a setting to redirect the user to the home view

--- backoffice/urls.py
+++ backoffice/urls.py
@@ -1,9 +1,14 @@
+from django.contrib.auth import views as auth_views
 from django.conf.urls import url
-
 from . import views
 
+
+login_args = {'template_name': 'backoffice/login.html'}
+
 urlpatterns = [
-    url(r'^login/$', views.login, name='backoffice-login'),
+    url(r'^login/$', auth_views.login, login_args,  name='backoffice_login'),
+    url(r'^logout/$', auth_views.logout, name='backoffice_logout'),
+    url(r'^home/$', views.home, name='backoffice_home'),
 ]
 
 
