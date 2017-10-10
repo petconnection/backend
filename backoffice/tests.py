3e363cd03335c2d1b451b98923fc2837d70f3ad4
Carlos Lucas <nosk14@gmail.com>
2017-10-11 00:10:59 +0200

    backoffiec login tests URL fixes

--- backoffice/tests.py
+++ backoffice/tests.py
@@ -11,17 +11,17 @@ class LoginTest(TestCase):
 
 
     def test_not_logged(self):
-        response = self.client.get('/')
-        self.assertRedirects(response, reverse('backoffice-login'))
+        response = self.client.get(reverse('backoffice_home'))
+        self.assertRedirects(response, reverse('backoffice_login'))
 
 
     def test_view(self):
-        response = self.client.get(reverse('backoffice-login'))
+        response = self.client.get(reverse('backoffice_login'))
         self.assertEquals(response.status_code, 200)
         self.assertEquals(response.templates[0].name, 'backoffice/login.html')
 
 
     def test_login(self):
-        response = self.client.post(reverse('backoffice-login'), {'username': 'testuser', 'password': '123'}, follow=True)
-        self.assertRedirects(response, '/')
+        response = self.client.post(reverse('backoffice_login'), {'username': 'testuser', 'password': '123'}, follow=True)
+        self.assertRedirects(response, reverse('backoffice_home'))
 
