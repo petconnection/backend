078d5a70f96e343b93706a3580d1f72e3efaab28
Carlos Lucas <nosk14@gmail.com>
2017-10-10 23:42:55 +0200

    logout redirects to login

--- backoffice/urls.py
+++ backoffice/urls.py
@@ -4,10 +4,11 @@ from . import views
 
 
 login_args = {'template_name': 'backoffice/login.html'}
+logout_args = {'next_page': 'backoffice_login'}
 
 urlpatterns = [
     url(r'^login/$', auth_views.login, login_args,  name='backoffice_login'),
-    url(r'^logout/$', auth_views.logout, name='backoffice_logout'),
+    url(r'^logout/$', auth_views.logout,logout_args, name='backoffice_logout'),
     url(r'^home/$', views.home, name='backoffice_home'),
 ]
 
