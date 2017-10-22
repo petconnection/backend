ffd4fc5c63077091fb2024221208dabf0820d29a
Carlos Lucas <nosk14@gmail.com>
2017-10-22 18:31:19 +0200

    Basic Home and CSS Added bootstram and made a basic Home view and template
    - Added bootstrap imports to base.html
    - Added styles to nav
    - Show list of animals of logged in user in home view

--- backoffice/views.py
+++ backoffice/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from api.models import Animal, Entity
 
 
 @login_required(login_url='backoffice_login')
 def home(request):
-    return render(request, 'backoffice/home.html', {})
+    context = {}
+    user_entity = Entity.objects.filter(user=request.user)[0]
+    context['animals'] = Animal.objects.filter(entity=user_entity)
+
+    return render(request, 'backoffice/home.html', context)
