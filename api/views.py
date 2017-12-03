703da4967af9d556f7a116446f35a3b6ac5e262b
Carlos Lucas <nosk14@gmail.com>
2017-12-03 20:12:35 +0100

    ViewSets permission specified
    Added AllowAny permission to ViewSet classes

--- api/views.py
+++ api/views.py
@@ -1,18 +1,22 @@
 from rest_framework import viewsets
 from api.models import Animal
 from api.serializers import *
+from rest_framework.permissions import AllowAny
 
 
 class AnimalViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Animal.objects.all()
     serializer_class = AnimalSerializer
+    permission_classes = [AllowAny]
 
 
 class EntityViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Entity.objects.all()
     serializer_class = EntitySerializer
+    permission_classes = [AllowAny]
 
 
 class MedicalRecordViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = MedicalRecord.objects.all()
     serializer_class = MedicalRecordSerializer
+    permission_classes = [AllowAny]
