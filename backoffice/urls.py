eff4c389a041ac8df60809f2e004d60070ccad53
Pablo <pablo.riutort@gmail.com>
2018-03-12 20:46:58 +0100

    remove home and simply use / as backoffice home

--- backoffice/urls.py
+++ backoffice/urls.py
@@ -6,6 +6,6 @@ from backoffice import views
 urlpatterns = [
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'},  name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
-    url(r'^home/$', views.home, name='home'),
+    url(r'^$', views.home, name='home'),
 ]
 
