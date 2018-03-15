a2ae4a9040053c8eb7e4f8ed84907e72df35c7a0
Pablo <pablo.riutort@gmail.com>
2018-03-15 12:51:29 +0100

    Remove unnecesary imports
    Remove auth import from legacy login function.
    Remove datetime import, the Animal model already handles this
    functionality

--- backoffice/views.py
+++ backoffice/views.py
@@ -2,13 +2,11 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 import api.models as models
 from .forms import AnimalForm
-from django.utils import timezone
 from django.http import JsonResponse as json
 
 
 @login_required(login_url='login')
 def home(request):
-    from django.contrib.auth import authenticate
     context = {}
     user_entity = models.Entity.objects.get(user=request.user)
     context['animals'] = models.Animal.objects.filter(entity=user_entity)
@@ -18,7 +16,6 @@ def home(request):
         form = AnimalForm(request.POST)
         if form.is_valid():
             r_breed = request.POST.get('breed')
-            print(request.POST)
             species = models.Species.objects.get(pk=request.POST['species'])
             if r_breed:
                 breed = models.Breed.objects.get_or_create(name=r_breed, species_field=species)[0]
@@ -32,7 +29,6 @@ def home(request):
                 size = form.cleaned_data['size'],
                 sex = form.cleaned_data['sex'],
                 bio = form.cleaned_data['bio'],
-                registration = timezone.now(),
                 breed_field = breed
             )
             
