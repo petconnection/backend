04fb9f41635ccd1970189f9b4ac21d776bde6aa4
Carlos Lucas <nosk14@gmail.com>
2018-03-17 19:15:18 +0100

    Added medical record api filters
    Now the API can filter animals by is_castraded and has_chip

--- api/filters.py
+++ api/filters.py
@@ -1,9 +1,17 @@
-from django_filters.rest_framework import FilterSet, NumberFilter
+from django_filters.rest_framework import FilterSet, NumberFilter, BooleanFilter
 from api.models import Animal
 
 
 class AnimalFilterSet(FilterSet):
     breed = NumberFilter(name='breed_field')
+    is_castrated = BooleanFilter(method='is_castrated_filter')
+    has_chip = BooleanFilter(method='has_chip_filter')
+
+    def is_castrated_filter(self, queryset, name, value):
+        return queryset.filter(medicalrecord__castrated=value)
+
+    def has_chip_filter(self, queryset, name, value):
+        return queryset.filter(medicalrecord__chip=value)
 
     class Meta:
         model = Animal
