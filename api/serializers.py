e3a60cea4f0ab4e60a9b45663da89ef12a158f55
Carlos Lucas <nosk14@gmail.com>
2018-03-17 22:13:31 +0100

    EntitySerializer renamed user to email

--- api/serializers.py
+++ api/serializers.py
@@ -12,21 +12,21 @@ class AnimalSerializer(serializers.ModelSerializer):
     breed = serializers.SerializerMethodField('get_breed_field')
     medical_record = MedicalRecordSerializer(read_only=True)
 
-    class Meta:
-        model = Animal
-        exclude = ['breed_field']
-
     @staticmethod
     def get_breed_field(obj):
         return obj.breed_field.id
 
+    class Meta:
+        model = Animal
+        exclude = ['breed_field']
+
 
 class EntitySerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.email')
+    email = serializers.ReadOnlyField(source='user.email')
 
     class Meta:
         model = Entity
-        fields = '__all__'
+        exclude = ['user']
 
 
 class SpeciesSerializer(serializers.ModelSerializer):
