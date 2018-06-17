c5dcbe1c8c7545ae502ff7193eff026dc0e1fc84
Carlos Lucas <nosk14@gmail.com>
2018-06-17 12:54:13 +0200

    removed clear button from animal form

--- backoffice/static/scripts.js
+++ backoffice/static/scripts.js
@@ -3,17 +3,6 @@ $(document).ready(function(){
 
   var frm = $('form');
 
-  $('#form-clear').click(function(){
-    frm.trigger('reset');
-    $('form input:radio').each(function(){
-      $(this).closest('label').removeClass('is-checked');
-    });
-
-    $('form input:checkbox').each(function(){
-      $(this).closest('label').removeClass('is-checked');
-    });
-  });
-
   $('#form-submit').click(function(){
     frm.submit();
   });
