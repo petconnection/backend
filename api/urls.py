f609cfc4a50981b896cd1916ab9c1e9bb91fc1c2
Carlos Lucas <nosk14@gmail.com>
2018-07-31 23:18:21 +0200

    Migration to Django 2.0.7
    - Updated dependencies versions
    - Added missing param on Entity model
    - Updated URLs to new format
    - Removed al migrations to start from the begining

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,15 @@
-from django.conf.urls import url, include
+from django.urls import path, include
 from api import views
 from rest_framework.routers import DefaultRouter
 
 
 router = DefaultRouter()
-router.register(r'animals', views.AnimalViewSet)
-router.register(r'entities', views.EntityViewSet)
-router.register(r'species', views.SpeciesViewSet)
-router.register(r'breeds', views.BreedViewSet)
+router.register('animals', views.AnimalViewSet)
+router.register('entities', views.EntityViewSet)
+router.register('species', views.SpeciesViewSet)
+router.register('breeds', views.BreedViewSet)
 
 
 urlpatterns = [
-        url(r'^', include(router.urls)),
+        path('', include(router.urls)),
     ]
