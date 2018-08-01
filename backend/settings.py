6c481856e95a0a9777fb272b0cf30f88cc8fd2ed
Carlos Lucas <nosk14@gmail.com>
2018-08-02 01:13:44 +0200

    added dirtydevelopers.org to allowed hosts

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'ob6*@n(7%mop)wd_ov978c%akm6=_i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.dirtydevelopers.org']
 
 
 # Application definition
