8636a525bcecccc53cd81938c674901aa549a37b
Carlos Lucas <nosk14@gmail.com>
2018-08-01 23:57:46 +0200

    default DB set to on cloud postgres

--- backend/settings.py
+++ backend/settings.py
@@ -88,9 +88,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'petconnection',
+        'USER': 'petconnection',
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': '92.222.81.25',
+        'PORT': '5432',
     }
+
 }
 
 
