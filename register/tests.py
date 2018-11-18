e3b22ee7eead3567142ad76bfd387b751e288838
Carlos Lucas <nosk14@gmail.com>
2018-11-18 01:11:43 +0100

    tests for entity registration

--- register/tests.py
+++ register/tests.py
@@ -1,3 +1,62 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from register.models import Token
+from django.utils import timezone
+from api.models import Entity
 
-# Create your tests here.
+
+class RegisterTests(TestCase):
+    def test_valid_token(self):
+        token = Token()
+        token.save()
+
+        client = Client()
+        response = client.get('/register/' + token.code + "/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_expired_token(self):
+        token = Token()
+        token.save()
+        token.expiry = timezone.now() - timezone.timedelta(hours=1)
+        token.save()
+
+        client = Client()
+        response = client.get('/register/' + token.code + "/")
+        self.assertEqual(response.status_code, 401)
+
+    def test_use_same_token_twice(self):
+        token = Token()
+        token.save()
+
+        form_data = {
+            "user_form-username": "RegisterTests_User",
+            "user_form-password1": "Password123",
+            "user_form-password2": "Password123",
+            "entity_form-name": "RegisterTests Entity",
+            "entity_form-location": "Test Location",
+        }
+
+        client = Client()
+        client.post('/register/' + token.code + "/", form_data)
+        entity_set = Entity.objects.filter(name=form_data["entity_form-name"])
+        self.assertEqual(len(entity_set), 1, "Entity not registered properly")
+
+        form_data["user_form-username"] += "_2"
+        form_data["entity_form-name"] += "_2"
+
+        client.post('/register/' + token.code + "/", form_data)
+        self.assertEqual(len(entity_set), 0, "Entity registered using an already used token")
+
+    def test_register_user_using_invalid_token(self):
+        form_data = {
+            "user_form-username": "RegisterTests_InvalidToken",
+            "user_form-password1": "Password123",
+            "user_form-password2": "Password123",
+            "entity_form-name": "RegisterTests InvalidToken",
+            "entity_form-location": "Test Location",
+        }
+
+        client = Client()
+        response = client.post('/register/abc/', form_data)
+        print(response.content)
+        entity_set = Entity.objects.filter(name=form_data["entity_form-name"])
+        self.assertEqual(len(entity_set), 0, "Entity registered using an unexisting token")
