7391f66327ba4a73c94a9c32f52008be6ae00b91
Pablo <pablo.riutort@gmail.com>
2018-12-13 17:05:31 +0100

    Modify Token
    Add consume method that sets expiry date to yerterday making token
    invalid
    Use specific import of datetime
    save() will check both expiry date and code before updating

--- register/models.py
+++ register/models.py
@@ -1,4 +1,4 @@
-from django.utils import timezone
+from django.utils.timezone import now, timedelta
 from django.db import models
 from secrets import token_urlsafe
 
@@ -7,16 +7,24 @@ class Token(models.Model):
     code = models.CharField(max_length=43)
     expiry = models.DateTimeField()
 
+
     def save(self, *args, **kwargs):
-        if not self.pk:
+        if not self.code:
             self.code = token_urlsafe()
-            self.expiry = timezone.now() + timezone.timedelta(days=1)
-        super(Token, self).save(*args, **kwargs)
+        if not self.expiry:
+            self.expiry = now() + timedelta(days=1)
+        return super(Token, self).save(*args, **kwargs)
 
 
     def is_valid(self):
-        return self.expiry > timezone.now()
+        return self.expiry > now()
+
+
+    def consume(self):
+        if self.expiry:
+            self.expiry -= timedelta(days=1)
 
 
     def __str__(self):
-        return "{} ({})".format(self.code, self.expiry)
+        return "{} [{}]".format(self.code, self.expiry)
+
